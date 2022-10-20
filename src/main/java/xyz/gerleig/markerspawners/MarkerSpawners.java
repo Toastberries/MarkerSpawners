60419cc5e3fe13326e45921449201ca9218ac719
Greg <46634118+gerleig@users.noreply.github.com>
2022-10-20 18:06:11 +0200

    made markerspawners unbreakable

--- src/main/java/xyz/gerleig/markerspawners/MarkerSpawners.java
+++ src/main/java/xyz/gerleig/markerspawners/MarkerSpawners.java
@@ -1,42 +1,81 @@
 package xyz.gerleig.markerspawners;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.SpawnerSpawnEvent;
-import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Set;
+
 
 public final class MarkerSpawners extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getLogger().info("asd");
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
     public void onSpawnerSpawnEvent(SpawnerSpawnEvent e) {
         Entity entity = e.getEntity();
+
+        if (!entity.getScoreboardTags().contains("MarkerSpawner")) return;
         World world = entity.getWorld();
-        //getLogger().info(entity.getScoreboardTags().toString());
+        Set<String> tags = entity.getScoreboardTags();
+        e.setCancelled(true);
 
-        if (entity.getScoreboardTags().contains("1-MarkerSpawner")) {
-            e.setCancelled(true);
+        String Id = "EXAMPLE_MOB";
+        EntityType Type = EntityType.ZOMBIE;
+        int SpawnLimit = 6;
+        double CheckDistance = 9;
+        EntityType PlaceholderType = EntityType.ARMOR_STAND;
+        double YOffset = 0;
 
-            Object[] args = entity.getScoreboardTags().toArray(); // FIX THIS
-            // 0: "MarkerSpawner" / 1: ID / 2: Type / 3: Amount / 4: Distance
-            String id = args[1].toString().substring(2);
-            EntityType type = EntityType.valueOf("STRAY"); // FIX THIS ALSO
-            int amount = Integer.parseInt(args[3].toString().substring(2));
-            double distance = Double.parseDouble(args[4].toString().substring(2));
-            getLogger().info(id + " " + type + " " + amount + " " + distance);
-
-            if (world.getNearbyEntities(entity.getLocation(), distance, distance, distance, (nearbyEntity) -> nearbyEntity.getType() == type).size() + 1 > amount) return;
-
-            world.spawnEntity(entity.getLocation().add(0, 1, 0), EntityType.ARMOR_STAND).addScoreboardTag(id);
+        for (String tag : tags) {
+            String[] split = tag.split("-");
+            switch (split[0]) {
+                case "Id" -> Id = split[1];
+                case "Type" -> Type = EntityType.valueOf(split[1]);
+                case "SpawnLimit" -> SpawnLimit = Integer.parseInt(split[1]);
+                case "CheckDistance" -> CheckDistance = Double.parseDouble(split[1]);
+                case "Placeholder" -> PlaceholderType = EntityType.valueOf(split[1]);
+                case "YOffset" -> YOffset = Double.parseDouble(split[1]);
+            }
         }
 
+        EntityType finalType = Type;
+        if (world.getNearbyEntities(entity.getLocation(), CheckDistance, CheckDistance, CheckDistance, (nearbyEntity) -> nearbyEntity.getType() == finalType).size() + 1 > SpawnLimit) return;
+        Entity spawn = world.spawnEntity(entity.getLocation().add(0, YOffset, 0), PlaceholderType, CreatureSpawnEvent.SpawnReason.CUSTOM);
+        spawn.addScoreboardTag("MarkerSpawner");
+        spawn.addScoreboardTag(Id);
+
     }
+
+    @EventHandler
+    public void onBlockBreakEvent(BlockBreakEvent e) {
+        if (isNotMarkerSpawner(e.getBlock())) return;
+        if (e.getPlayer().hasPermission("markerspawners.break")) return;
+        e.setCancelled(true);
+        e.getPlayer().sendMessage("This spawner has a powerful aura, it doesn't seem breakable.");
+    }
+    @EventHandler
+    public void onBlockExplodeEvent(BlockExplodeEvent e) {
+        if (isNotMarkerSpawner(e.getBlock())) return;
+        e.setCancelled(true);
+    }
+
+    public boolean isNotMarkerSpawner(Block block) {
+        if (block.getType() != Material.SPAWNER) return true;
+        else return ((CreatureSpawner) block.getState()).getSpawnedType() != EntityType.MARKER;
+    }
+
 }
